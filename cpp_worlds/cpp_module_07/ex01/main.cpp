d6a46453a4403f35b9284cbebf0814b2c4ed186d
jhdgo1225 <94268538+jhdgo1225@users.noreply.github.com>
2024-05-17 04:12:58 +0000

    CPP07 ex01 is updated

--- cpp_worlds/cpp_module_07/ex01/main.cpp
+++ cpp_worlds/cpp_module_07/ex01/main.cpp
@@ -9,7 +9,11 @@ int main(void)
 {
     int arr[3] = {1, 2, 3};
     ::iter(arr, 3, print);
+    ::iter(arr, 3, plus);
+    ::iter(arr, 3, print);
     std::string arr2[3] = {"abc", "def", "ghi"};
+    ::iter(arr2, 3, printModule);
+    ::iter(arr2, 3, plus);
     ::iter(arr2, 3, printModule);
     return (0);
 }
