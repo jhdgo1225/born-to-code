a47ae5f20f476f84ad344fffd2ce23b26fc505fe
jhdgo1225 <94268538+jhdgo1225@users.noreply.github.com>
2024-05-26 14:11:40 +0000

    CPP09 ex02 Merge-Insertion Sort With Vector is Done

--- cpp_worlds/cpp_module_09/ex02/PmergeMe.cpp
+++ cpp_worlds/cpp_module_09/ex02/PmergeMe.cpp
@@ -1,6 +1,7 @@
 #include "PmergeMe.hpp"
 #include <ctime>
 
+int cnt;
 PmergeMe::PmergeMe(int argc, char **argv)
 {
     for (int idx = 1; idx < argc; idx++)
@@ -30,24 +31,79 @@ PmergeMe& PmergeMe::operator=(const PmergeMe& rhs)
     return (*this);
 }
 
-int lowerBoundWithVector(std::vector<int>& vc, int key) {
-	int low = 0;
-	int high = vc.size();
-	while(low < high) {
-		int mid = (low + high) / 2;
-		if (key <= vc[mid])
-			high = mid;
-		else
-			low = mid + 1;
-	}
-	return low;
-}
-
 void    printElements(std::vector<std::pair<int, int> >& vc)
 {
     for (std::vector<std::pair<int, int> >::iterator iter = vc.begin(); iter != vc.end(); iter++)
         std::cout << iter->first << ' ';
     std::cout << '\n';
+}
+
+std::vector<std::pair<int, int> >::iterator lowerBoundVector(std::vector<std::pair<int, int> >& vc, std::vector<std::pair<int, int> >::iterator s, std::vector<std::pair<int, int> >::iterator e, std::pair<int, int> key)
+{
+    std::vector<std::pair<int, int> >::iterator retIter = vc.begin();
+    int low = 0;
+    int high = e - s;
+    int mid;
+    while (low < high)
+    {
+        cnt++;
+        mid = (low + high) / 2;
+        if (key.first <= vc[mid].first)
+            high = mid;
+        else
+            low = mid + 1;
+    }
+    for (int idx = 0; idx < low; idx++)
+        retIter++;
+    return (retIter);
+}
+
+void insertInVector(std::vector<std::pair<int, int> >& mainChain, std::vector<std::pair<int, int> >& pendingElements, int vcSize)
+{
+    std::vector<std::pair<int, int> > newMainChain;
+    std::vector<std::pair<int, int> > tmp;
+    std::vector<std::pair<int, int> >::iterator endIter;
+    std::vector<std::pair<int, int> >::iterator InsertionIter;
+    int n1 = 0, n2 = 1, end = 0, start = 1;
+    int mainChainSize = mainChain.size();
+    int pendingElementsSize = pendingElements.size();
+    newMainChain.push_back(pendingElements[0]);
+    newMainChain.push_back(mainChain[0]);
+    while (vcSize != static_cast<int>(newMainChain.size()))
+    {
+        n1 = n2;
+        n2 = start;
+        end = start;
+        start = n1 * 2 + n2;
+        if (start > pendingElementsSize)
+            start = pendingElementsSize;
+        for (int idx2 = end; (idx2 < start && idx2 < mainChainSize); idx2++)
+            newMainChain.push_back(mainChain[idx2]);
+        for (int idx2 = start; idx2 > end; idx2--)
+        {
+            if (newMainChain.back().first <= pendingElements[idx2 - 1].first)
+            {
+                tmp.push_back(pendingElements[idx2 - 1]);
+                continue ;
+            }
+            endIter = newMainChain.end();
+            endIter--;
+            InsertionIter = newMainChain.insert(lowerBoundVector(newMainChain, newMainChain.begin(), endIter, pendingElements[idx2 - 1]), pendingElements[idx2 - 1]);
+            tmp.push_back(newMainChain.back());
+            newMainChain.pop_back();
+            if (InsertionIter - newMainChain.begin() + 1 == newMainChain.end() - newMainChain.begin())
+            {
+                tmp.push_back(newMainChain.back());
+                newMainChain.pop_back();
+            }
+        }
+        while (!tmp.empty())
+        {
+            newMainChain.push_back(tmp.back());
+            tmp.pop_back();
+        }
+    }
+    mainChain = newMainChain;
 }
 
 void    mergeInsertionVector(std::vector<std::pair<int, int> >& vc)
@@ -60,6 +116,7 @@ void    mergeInsertionVector(std::vector<std::pair<int, int> >& vc)
     std::vector<std::pair<int, int> >::iterator vcIter = vc.begin();
     for (int idx=0; idx < (vcSize >> 1); idx++)
     {
+        cnt++;
         std::pair<int, int> mainChainItem = *vcIter++;
         std::pair<int, int> pendingElementsItem = *vcIter++;
         if (mainChainItem.first < pendingElementsItem.first)
@@ -71,27 +128,31 @@ void    mergeInsertionVector(std::vector<std::pair<int, int> >& vc)
         mainChain.push_back(mainChainItem);
         pendingElements.push_back(pendingElementsItem);
     }
-    mergeInsertionVector(mainChain);
+    std::vector<std::pair<int, int> > updatedMainChain;
+    std::vector<std::pair<int, int> > updatedPendingElements;
+    for (std::vector<std::pair<int, int> >::iterator it = mainChain.begin(); it != mainChain.end(); it++)
+        updatedMainChain.push_back(std::pair<int, int>(it->first, it - mainChain.begin()));
+    mergeInsertionVector(updatedMainChain);
     for (int idx = 0; idx < (vcSize >> 1); idx++)
-        pendingElements.push_back(pendingElements[mainChain[idx].second]);
-    pendingElements.erase(pendingElements.begin(), pendingElements.begin() + (vcSize >> 1));
+    {   
+        int updatedIdx = updatedMainChain[idx].second;
+        updatedPendingElements.push_back(pendingElements[updatedIdx]);
+        updatedMainChain[idx].second = mainChain[updatedIdx].second;
+    }
     if (vcSize % 2)
-        pendingElements.push_back(*vcIter);
-    
-    vc = mainChain;
+        updatedPendingElements.push_back(*vcIter);
+    insertInVector(updatedMainChain, updatedPendingElements, vcSize);
+    vc = updatedMainChain;
 }
 
 void PmergeMe::mergeInsertionSort()
 {
     std::cout << "Before: ";
     printElements(vc);
-    // time 변수 추가 후 'Vector' Merge-Insertion Sort 시간 측정 시작
     clock_t startVector = clock();
-    // 'Vector' Merge-Insertion Sort 수행
     mergeInsertionVector(vc);
-    // 'Vector' Merge-Insertion Sort 시간 측정 종료 후 소요시간 저장
     clock_t endVector = clock();
-    double  diffTimeVector = difftime(endVector, startVector) * 1000;
+    double  diffTimeVector = endVector - startVector;
     // =========================================================
     // 'List' Merge-Insertion Sort 시간 측정 시작
     clock_t startList = clock();
@@ -99,9 +160,10 @@ void PmergeMe::mergeInsertionSort()
     // mergeInsertionList(li);
     // 'List' Merge-Insertion Sort 시간 측정 종료 후 소요시간 저장
     clock_t endList = clock();
-    double  diffTimeList = difftime(endList, startList) * 1000;
+    double  diffTimeList = endList - startList;
     std::cout << "After: ";
     printElements(vc);
-    std::cout << "Time to process a range of " << vc.size() << " elements with std::vector : " << diffTimeVector << " us" << '\n';
-    std::cout << "Time to process a range of " << li.size() << " elements with std::list : " << diffTimeList << " us" << '\n';
+    std::cout << "Time to process a range of " << vc.size() << " elements with std::vector : " << diffTimeVector / CLOCKS_PER_SEC << " s" << '\n';
+    std::cout << "Time to process a range of " << li.size() << " elements with std::list : " << diffTimeList << " s" << '\n';
+    std::cout << cnt << '\n';
 }
